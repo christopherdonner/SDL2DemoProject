6b902ba54129cc937ade9aedd9e5a460b08760af
Christopher Donner <cdonner@opentext.com>
2025-02-05 13:31:24 -0500

    animation init

--- 01_hello_SDL.cpp
+++ 01_hello_SDL.cpp
@@ -10,10 +10,12 @@ and may not be redistributed without written permission.*/
 #include <vector>
 #include <chrono>
 
+using std::vector;
+using std::pair;
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 640,
-		  SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 320,
+		  SCREEN_HEIGHT = 240;
 
 int main( int argc, char* args[] )
 {
@@ -22,7 +24,7 @@ int main( int argc, char* args[] )
 	GPU_Target* window = GPU_InitRenderer(GPU_RENDERER_OPENGL_3, SCREEN_WIDTH, SCREEN_HEIGHT, GPU_DEFAULT_INIT_FLAGS);
 	GPU_Image* hero = GPU_LoadImage("adventurer-sheet.png");
 
-	std::vector<GPU_Rect> rects;
+	vector<GPU_Rect> rects;
 	size_t nbRow = 11;
 	size_t nbCol = 7;
 	size_t widthSpr = 50;
@@ -34,24 +36,24 @@ int main( int argc, char* args[] )
 		}
 	}
 
-	std::vector<std::pair<size_t, size_t>> idle1{ {0, 0}, {0, 1}, {0, 2}, {0, 3} };
-	std::vector<std::pair<size_t, size_t>> crouch{ {0, 4}, {0, 5}, {0, 6}, {1, 0} };
-	std::vector<std::pair<size_t, size_t>> run{ {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6} };
-	std::vector<std::pair<size_t, size_t>> jump{ {2, 0}, {2, 1}, {2, 2}, {2, 3} };
-	std::vector<std::pair<size_t, size_t>> mid{ {2, 4}, {2, 5}, {2, 6}, {3, 0} };
-	std::vector<std::pair<size_t, size_t>> fall{ {3, 1}, {3, 2} };
-	std::vector<std::pair<size_t, size_t>> slide{ {3, 3}, {3, 4}, {3, 5}, {3, 6}, {4, 0} };
-	std::vector<std::pair<size_t, size_t>> grab{ {4, 1}, {4, 2}, {4, 3}, {4, 4} };
-	std::vector<std::pair<size_t, size_t>> climb{ {4, 5}, {4, 6}, {5, 0}, {5, 1}, {5, 2} };
-	std::vector<std::pair<size_t, size_t>> idle2{ {5, 3}, {5, 4}, {5, 5}, {5, 6} };
-	std::vector<std::pair<size_t, size_t>> attack1{ {6, 0}, {6, 1}, {6, 2}, {6, 3}, {6, 4} };
-	std::vector<std::pair<size_t, size_t>> attack2{ {6, 5}, {6, 6}, {7, 0}, {7, 1}, {7, 2}, {7, 3} };
-	std::vector<std::pair<size_t, size_t>> attack3{ {7, 4}, {7, 5}, {7, 6}, {8, 0}, {8, 1}, {8, 2} };
-	std::vector<std::pair<size_t, size_t>> hurt{ {8, 3}, {8, 4}, {8, 5} };
-	std::vector<std::pair<size_t, size_t>> die{ {8, 6}, {9, 0}, {9, 1}, {9, 2}, {9, 3}, {9, 4}, {9, 5} };
-	std::vector<std::pair<size_t, size_t>> jump2{ {9, 6}, {10, 0}, {10, 1} };
+	vector<pair<size_t, size_t>> idle1{ {0, 0}, {0, 1}, {0, 2}, {0, 3} };
+	vector<pair<size_t, size_t>> crouch{ {0, 4}, {0, 5}, {0, 6}, {1, 0} };
+	vector<pair<size_t, size_t>> run{ {1, 1}, {1, 2}, {1, 3}, {1, 4}, {1, 5}, {1, 6} };
+	vector<pair<size_t, size_t>> jump{ {2, 0}, {2, 1}, {2, 2}, {2, 3} };
+	vector<pair<size_t, size_t>> mid{ {2, 4}, {2, 5}, {2, 6}, {3, 0} };
+	vector<pair<size_t, size_t>> fall{ {3, 1}, {3, 2} };
+	vector<pair<size_t, size_t>> slide{ {3, 3}, {3, 4}, {3, 5}, {3, 6}, {4, 0} };
+	vector<pair<size_t, size_t>> grab{ {4, 1}, {4, 2}, {4, 3}, {4, 4} };
+	vector<pair<size_t, size_t>> climb{ {4, 5}, {4, 6}, {5, 0}, {5, 1}, {5, 2} };
+	vector<pair<size_t, size_t>> idle2{ {5, 3}, {5, 4}, {5, 5}, {5, 6} };
+	vector<pair<size_t, size_t>> attack1{ {6, 0}, {6, 1}, {6, 2}, {6, 3}, {6, 4} };
+	vector<pair<size_t, size_t>> attack2{ {6, 5}, {6, 6}, {7, 0}, {7, 1}, {7, 2}, {7, 3} };
+	vector<pair<size_t, size_t>> attack3{ {7, 4}, {7, 5}, {7, 6}, {8, 0}, {8, 1}, {8, 2} };
+	vector<pair<size_t, size_t>> hurt{ {8, 3}, {8, 4}, {8, 5} };
+	vector<pair<size_t, size_t>> die{ {8, 6}, {9, 0}, {9, 1}, {9, 2}, {9, 3}, {9, 4}, {9, 5} };
+	vector<pair<size_t, size_t>> jump2{ {9, 6}, {10, 0}, {10, 1} };
 
-	std::vector<std::pair<size_t, size_t>> current = idle1;
+	vector<pair<size_t, size_t>> current = idle1;
 	size_t index = 0;
 
 	double maxDuration = 150;
@@ -131,7 +133,7 @@ int main( int argc, char* args[] )
 		GPU_Clear(window);
 		auto currentPair = current[index];
 		size_t position = currentPair.second + currentPair.first * nbCol;
-		GPU_BlitTransformX(hero, &rects[position], window, 200, 200, 0, 0, 0, 1, 1);
+		GPU_BlitTransformX(hero, &rects[position], window, 120, 120, 0, 0, 0, 1, 1);
 		GPU_Flip(window);
 
 		timeBuffer = timeBuffer + timeElapsed;
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,30 +1,80 @@
 #include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+#include <chrono>
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_gpu.h>
 
 using namespace std;
 
-
-
-void testFunction() 
+int main(int argc, char* args[])
 {
-	cout << "this is the test function" << endl;
-}
+	SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS);
 
-int main(void)
-{
-	int age = 43;
-	cout << boolalpha;
-	cout << (age >= 21) << endl;
+	GPU_Target* window = GPU_InitRenderer(GPU_RENDERER_OPENGL_3, 320, 240, GPU_DEFAULT_INIT_FLAGS);
+	GPU_Image* player = GPU_LoadImage("advenurer-sheet.png");
 
-	if (age < 65) {
-		cout << "not yet retirement age"<<endl;
-	}
-	for (int i = 0; i < 10; i++) {
-		cout << i << endl;
-	}
-	testFunction();
+	vector<GPU_Rect> rects;
+	size_t numRow = 11;
+	size_t numCol = 7;
+	size_t spriteWidth = 50;
+	size_t spriteHeight = 37;
 
 	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "hello world", "Successful", NULL);
+
+	for (int i = 0; i< numRow; i++)
+	{
+		for (int j = 0; i < numCol; j++)
+		{
+			rects.push_back(GPU_Rect{ (float)(j * spriteWidth), (float)(i * spriteHeight), (float)(spriteWidth), (float)(spriteWidth)});
+		}
+	}
+
+	vector<pair<size_t, size_t>> idle1{ {0,0}, {0,1},{0,2},{0,3} };
+
+	vector<pair<size_t, size_t>> current = idle1;
+	size_t index = 0;
+	int maxDuration = 150,
+		timeBuffer = 0,
+		timeElapsed = 0;
+	SDL_Event event;
+
+	bool done = false;
+
+	while (!done) 
+	{
+		typedef chrono::high_resolution_clock Clock;
+		short elapsedNano = 0;
+		auto t1 = Clock::now();
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT)
+			{
+				done = true;
+			}
+			index = 0;
+		}
+	}
+
+	GPU_Clear(window);
+	auto currentPair = current[index];
+	size_t position = currentPair.second + currentPair.first * numCol;
+	GPU_BlitTransformX(player, &rects[position], window, 120, 120, 0, 0, 0, 1, 1);
+
+	timeBuffer += timeElapsed;
+
+	if(timeBuffer > maxDuration)
+	{
+		timeBuffer = 0;
+		index++;
+
+		if (index >= current.size())
+		{
+			index = 0;
+		}
+	}
 
 	return 0;
 }
